127b00a7a000c623dadf0383893140407d24a793
Joshua Henize <jlhenize.webreg@gmail.com>
2024-08-13 04:57:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import parser
 import lexer
 
-#Parser(lexer.Lexer("1+1").make_tokens()).parse()
+#Parser(lexer.Lexer("1+1").make_tokens()).parse()[-1]
 
-ast = parser.Parser(lexer.Lexer("(-1 - -3) * (2 + 3) / 2 + 1").make_tokens()).parse()
+#the last element in the list returned is the final AST
+ast = parser.Parser(lexer.Lexer("(-1 - -3) * (2 + 3) / 2 + 1").make_tokens()).parse()[-1]
 print (ast)
