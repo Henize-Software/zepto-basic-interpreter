432fbd26785cbd30eb160a5ee1fa9767084cff17
Joshua Henize <jlhenize.webreg@gmail.com>
2024-08-14 01:41:49 -0400

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,11 +1,16 @@
-#Created 2024 Joshua Henize
+#created 2024 Joshua Henize
 import lexer
 
 class Node:
+    token_or_node = None
     def __init__(self):
         pass
+    def get_pval(self):
+        return self.token_or_node
+    def set_pval(self, value):
+        self.token_or_node = value
 
-class NumberNode:
+class NumberNode(Node):
     def __init__(self, token_or_node) -> None:
         if isinstance(token_or_node, NumberNode):
             self.token_or_node = token_or_node.token_or_node
@@ -13,20 +18,14 @@ class NumberNode:
             self.token_or_node = token_or_node
     def __repr__(self) -> str:
         return f"n_n({self.token_or_node})"
-        
-    def get_pval(self):
-        return self.token_or_node
-    def set_pval(self, value):
-        self.token_or_node = value
-
-class BinaryOPNode: 
+    
+class BinaryOPNode(Node): 
     def __init__(self, op_token, left_number_node, right_number_node) -> None:
         self.op_token = op_token
         self.left_number_node = left_number_node
         self.right_number_node = right_number_node
     def __repr__(self) -> str:
         return f"n_op({self.left_number_node} {self.op_token} {self.right_number_node})"
-        
     def get_pval(self):
         return self.right_number_node
     def set_pval(self, value):
@@ -41,49 +40,49 @@ class Parser:
         self.tokens = tokens
 
     def parse(self) -> list:
-        expression_scratch_pad = []
-        while self.parse_next(expression_scratch_pad) is not None:
+        expr_buff = []
+        while self.parse_next(expr_buff) is not None:
             pass
         print ("break")
-        return expression_scratch_pad
+        return expr_buff[-1]
         
-    def parse_next(self, expression_scratch_pad) -> Node:
+    def parse_next(self, expr_buff) -> Node:
         if self.next_token() is None:
             return None
-        if expression_scratch_pad is None:
-            expression_scratch_pad = []
+        if expr_buff is None:
+            expr_buff = []
             
         if self.current_token.type == lexer.T_LPAREN:
-            expression_scratch_pad.append(NumberNode(self.parse()[-1]))
+            expr_buff.append(NumberNode(self.parse()))
         elif self.current_token.type == lexer.T_RPAREN:
             return None
         elif self.current_token.type in (lexer.T_INT, lexer.T_FLOAT):
-            expression_scratch_pad.append(NumberNode(self.current_token))
-
-        elif self.current_token.type in (lexer.T_PLUS, lexer.T_MINUS): #also handles unary op -5 + 1 or 3 + -5
-            left_node = expression_scratch_pad[-1] if len(expression_scratch_pad) > 0  \
-                                                              else \
-                                                                 lexer.Token(lexer.T_INT, 0)
-            expression_scratch_pad.append(BinaryOPNode(self.current_token,  \
-                                                   NumberNode(left_node), \
-                                                   self.parse_next(None)))
+            expr_buff.append(NumberNode(self.current_token))
+        
+        #also handles unary op -5 + 1 or 3 + -5
+        elif self.current_token.type in (lexer.T_PLUS, lexer.T_MINUS): 
+            if len(expr_buff) > 0:
+                left_node = expr_buff[-1]
+            else:
+                left_node = lexer.Token(lexer.T_INT, 0)
+            new_node = BinaryOPNode(self.current_token,  \
+                                    NumberNode(left_node),\
+                                    self.parse_next(None))
+            expr_buff.append(new_node)
                     
         elif self.current_token.type in (lexer.T_DIV, lexer.T_MUL):
-            expression_scratch_pad.append(BinaryOPNode(self.current_token,  \
-                                                   NumberNode(expression_scratch_pad[-1].get_pval()),  \
-                                                   self.parse_next(None)))
-            expression_scratch_pad[-2].set_pval(expression_scratch_pad[-1])
-            expression_scratch_pad.append(expression_scratch_pad[-2])
+            new_node = BinaryOPNode(self.current_token,  \
+                                    NumberNode(expr_buff[-1].get_pval()),  \
+                                    self.parse_next(None))
+            expr_buff.append(new_node)
+            expr_buff[-2].set_pval(expr_buff[-1])
+            expr_buff.append(expr_buff[-2])
             
 
-        return expression_scratch_pad[-1]
-       
-   
-            
+        return expr_buff[-1]
+
     def next_token(self):
         self.token_idx += 1
         self.current_token = self.tokens[self.token_idx] if self.token_idx < len(self.tokens) else None
         return self.current_token
     
-     
-
